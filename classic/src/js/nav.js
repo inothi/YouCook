062dacdfc7d19e2a67f4880afd118fe2dc2c4a4d
inothi <inothi.tomek@gmail.com>
2024-03-21 13:45:25 +0100

    adding blinking for important link from navbar.

--- classic/src/js/nav.js
+++ classic/src/js/nav.js
@@ -1,7 +1,6 @@
 // adding class 'active-link' for link from navbar 
 function activeLink(elem) {
     let changeClass = document.getElementsByTagName("a");
-    console.log(changeClass);
     for (let i = 0; i < changeClass.length; i++) {
         changeClass[i].classList.remove("active-link");
     }
